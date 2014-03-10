f4e260959c462207b6bc032313a662d9d04a19b1
Sameer Siruguri <sameer@dstrategies.org>
2014-03-10 17:25:50 +0000

    first commit of orig theme

---
+++ footer.php
@@ -0,0 +1,14 @@
+  <sidebar>	
+  <?php get_sidebar( 'footer' ); ?>
+</sidebar>
+  <footer>
+    <line></line>
+    <p>Design by <a href="http://www.dstrategies.org/">Digital Strategies</a></p>
+  </footer>
+
+  </div>
+  
+  <?php wp_footer(); ?>
+      
+</body>
+</html>
